2969b5165a8c0662c6674c1a823367c1246e442e
Amrutha <varshiniveerabathula@gmail.com>
2026-07-27 23:44:47 +0530

    ffff

--- pages/login.js
+++ pages/login.js
@@ -3,14 +3,22 @@ function login() {
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value.trim();
 
+    console.log("Button clicked");
+
     if (username === "PAPA" && password === "0506") {
 
-        alert("Welcome ❤️");
-        window.location.href = "../index.html";
+        console.log("Correct Password");
+
+        sessionStorage.setItem("loggedIn", "true");
+
+        console.log(sessionStorage.getItem("loggedIn"));
+
+        window.location.replace("../index.html");
 
     } else {
 
         alert("Wrong Name or Password 💔");
 
     }
+
 }
